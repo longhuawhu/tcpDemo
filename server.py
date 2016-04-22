c3046b91b8b7baf5760bcca536e472348600ac5c
lhwhu <longhua1949hm@163.com>
2016-04-22 15:39:48 +0800

    修改 server.py

--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 __author__ = 'lh'
 
 import socket
+import threading
+import time
+
+def tcpAction(sock, addr):
+    print('accept new connection from ', addr)
+    sock.send(b'welcome')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        else:
+            print data;
+            data = 'echo: ' + data;
+            print data
+            sock.send((b'%s!\x0D\x0A' % data.decode('utf-8')).encode('utf-8'))
+
 
 host = "localhost"
-port = 8888
+port = 53883
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-s.listen(10);
+s.listen(10)
+
+
 while 1:
     sock, addr = s.accept()
-    print "get connection form", sock.getpeername
-    data = sock.recv(1024)
-    if not data:
-        break
-    else:
-        print  data
-        data = "send from server" + data
-        sock.send(data)
+    t = threading.Thread(target = tcpAction, args = (sock, addr))
+    t.start()
+
 
